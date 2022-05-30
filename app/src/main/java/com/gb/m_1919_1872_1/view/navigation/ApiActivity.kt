a2217c706f007d19f8d275d5b6c5ac5fbe1bcbdf
Andej Nesterenko <mentatus.n@gmail.com>
2022-05-30 21:03:40 +0300

    Урок 3 | познакомились с ViewPager

--- app/src/main/java/com/gb/m_1919_1872_1/view/navigation/ApiActivity.kt
+++ app/src/main/java/com/gb/m_1919_1872_1/view/navigation/ApiActivity.kt
@@ -16,11 +16,11 @@ class ApiActivity : AppCompatActivity() {
         binding = ActivityApiBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-        //binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
 
-       // binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
-       // binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_system)
-        //binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_mars)
+        binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
+        binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_system)
+        binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_mars)
         //binding.textOne.setTextColor(resources.getColor(R.color.red))
        // binding.textOne.setTextColor(resources.getColor(R.color.blu))
     }
