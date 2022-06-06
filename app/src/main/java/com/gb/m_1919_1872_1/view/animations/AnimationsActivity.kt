cb341db423f8a8b37a4549e0d974d37f6794909c
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-06 22:20:39 +0300

    урок 5 | StateListAnimator

--- app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
@@ -1,21 +1,12 @@
 package com.gb.m_1919_1872_1.view.animations
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.*
-import com.gb.m_1919_1872_1.R
 import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
+
+    private val duration: Long = 1000
 
     var isOpen: Boolean = false
     private lateinit var binding: ActivityAnimationsBinding
@@ -23,16 +14,11 @@ class AnimationsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            isOpen = !isOpen
-            val transition = ChangeBounds()
-            val path = ArcMotion()
-            transition.setPathMotion(path)
-            transition.duration = 3000
-            TransitionManager.beginDelayedTransition(binding.root,transition)
-            val params = (binding.button.layoutParams as FrameLayout.LayoutParams)
-            params.gravity = if(isOpen){ Gravity.BOTTOM or Gravity.END}else { Gravity.TOP or Gravity.START}
-            binding.button.layoutParams = params
+
+        /*binding.header.isSelected = true
+        binding.header.isSelected = false*/
+        binding.scrollView.setOnScrollChangeListener { _, _, _, _, _ ->
+            binding.header.isSelected = binding.scrollView.canScrollVertically(-1)
         }
     }
 
