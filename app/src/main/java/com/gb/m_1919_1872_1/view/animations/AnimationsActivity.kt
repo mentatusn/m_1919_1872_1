185381e1b4377c3c309fbe79db8081384a34171f
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-06 23:14:11 +0300

    урок 5 | ConstraintSet анимация по желанию Кирилла

--- app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
@@ -4,43 +4,47 @@ import android.os.Bundle
 import android.view.animation.AnticipateOvershootInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
+import androidx.transition.*
 import com.gb.m_1919_1872_1.R
 import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBinding
 import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBonusStartBinding
+import com.gb.m_1919_1872_1.databinding.ActivityAnimationsKirillBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
     private val duration: Long = 1000
 
     var isOpen: Boolean = false
-    private lateinit var binding: ActivityAnimationsBonusStartBinding
+    private lateinit var binding: ActivityAnimationsKirillBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsBonusStartBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsKirillBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.backgroundImage.setOnClickListener {
+        val constraintSetStart = ConstraintSet()
+        val constraintSetEnd = ConstraintSet()
+        constraintSetStart.clone(this,R.layout.include_fab_animations_start)
+        constraintSetEnd.clone(this,R.layout.include_fab_animations_end)
+        binding.anim.fabCenter.setOnClickListener {
 
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(binding.constraintContainer)
 
-            val transition = ChangeBounds()
-            transition.interpolator = AnticipateOvershootInterpolator(5f)
-            transition.duration = 1000
-            TransitionManager.beginDelayedTransition(binding.constraintContainer,transition )
+
+            val transitionSet = TransitionSet()
+            val transitionCB = ChangeBounds()
+            val transitionFade = Fade()
+            transitionSet.addTransition(transitionCB)
+            transitionSet.addTransition(transitionFade)
+            transitionSet.interpolator = AnticipateOvershootInterpolator(5f)
+            transitionSet.duration = 1000
+            TransitionManager.beginDelayedTransition(binding.anim.container,transitionSet )
 
             isOpen = !isOpen
-            if(isOpen){
-                //constraintSet.clear(R.id.title)
-                constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.RIGHT)
+            if(!isOpen){
+                constraintSetStart.applyTo(binding.anim.container)
             }else{
-                constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.LEFT)
+                constraintSetEnd.applyTo(binding.anim.container)
             }
 
-
-            constraintSet.applyTo(binding.constraintContainer)
         }
     }
 
