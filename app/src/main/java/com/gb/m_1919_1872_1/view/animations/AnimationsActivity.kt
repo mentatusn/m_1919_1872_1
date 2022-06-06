dccdbfb67e145547254be57ff4edaecabc176c37
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-06 20:33:04 +0300

    урок 5 начали | познакомились с TransitionManager

---
+++ app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
@@ -0,0 +1,42 @@
+package com.gb.m_1919_1872_1.view.animations
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.*
+import com.gb.m_1919_1872_1.R
+import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBinding
+import com.gb.m_1919_1872_1.view.picture.PictureOfTheDayFragment
+
+class AnimationsActivity : AppCompatActivity() {
+
+    var isOpen: Boolean = false
+    private lateinit var binding: ActivityAnimationsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAnimationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            //TransitionManager.beginDelayedTransition(binding.transitionsContainer)
+            val transitionFade = Fade()
+            transitionFade.duration = 3000
+            val transitionChangeBounds = ChangeBounds()
+            transitionChangeBounds.duration= 5000
+            val transitionSet = TransitionSet()
+            transitionSet.addTransition(transitionFade)
+            transitionSet.addTransition(transitionChangeBounds)
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer,transitionSet)
+            //TransitionManager.beginDelayedTransition(binding.transitionsContainer,transitionFade)
+            //TransitionManager.beginDelayedTransition(binding.transitionsContainer,transitionChangeBounds)
+            isOpen = !isOpen
+            binding.text.visibility = if (isOpen) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+
+        }
+
+    }
+}
