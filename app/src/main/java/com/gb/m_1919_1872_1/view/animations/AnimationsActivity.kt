13f42056ce43b52dca327c3c3188712272acdb4b
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-07 16:04:00 +0300

    урок 5 закончили | познакомились ObjectAnimator и View.animate() (забыли запушить, и потеряли написанный на уроке код, пришлось писать еще раз)

--- app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/m_1919_1872_1/view/animations/AnimationsActivity.kt
@@ -1,52 +1,86 @@
 package com.gb.m_1919_1872_1.view.animations
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.animation.AnticipateOvershootInterpolator
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.transition.*
-import com.gb.m_1919_1872_1.R
-import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBinding
-import com.gb.m_1919_1872_1.databinding.ActivityAnimationsBonusStartBinding
-import com.gb.m_1919_1872_1.databinding.ActivityAnimationsKirillBinding
+import com.gb.m_1919_1872_1.databinding.ActivityAnimationsRotateFabBinding
 
 class AnimationsActivity : AppCompatActivity() {
+    val duration = 1000L
 
-    private val duration: Long = 1000
-
-    var isOpen: Boolean = false
-    private lateinit var binding: ActivityAnimationsKirillBinding
+    lateinit var binding: ActivityAnimationsRotateFabBinding
+    var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsKirillBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsRotateFabBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val constraintSetStart = ConstraintSet()
-        val constraintSetEnd = ConstraintSet()
-        constraintSetStart.clone(this,R.layout.include_fab_animations_start)
-        constraintSetEnd.clone(this,R.layout.include_fab_animations_end)
-        binding.anim.fabCenter.setOnClickListener {
+        binding.fab.setOnClickListener {
+            flag = !flag
+            if (flag) {
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 0f, 405f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -50f, -260f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -20f, -130f)
+                    .setDuration(duration).start()
 
+                binding.optionOneContainer.animate()
+                    .alpha(1f)
+                    .setDuration(duration / 2)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionOneContainer.isClickable = true
+                        }
+                    })
+                binding.optionTwoContainer.animate()
+                    .alpha(1f)
+                    .setDuration(duration / 2)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionTwoContainer.isClickable = true
+                        }
+                    })
 
+                binding.transparentBackground.animate()
+                    .alpha(0.5f)
+                    .setDuration(duration)
+            } else {
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 405f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -260f, -50f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -130f, -20f)
+                    .setDuration(duration).start()
 
-            val transitionSet = TransitionSet()
-            val transitionCB = ChangeBounds()
-            val transitionFade = Fade()
-            transitionSet.addTransition(transitionCB)
-            transitionSet.addTransition(transitionFade)
-            transitionSet.interpolator = AnticipateOvershootInterpolator(5f)
-            transitionSet.duration = 1000
-            TransitionManager.beginDelayedTransition(binding.anim.container,transitionSet )
+                binding.optionOneContainer.animate()
+                    .alpha(0f)
+                    .setDuration(duration / 2)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionOneContainer.isClickable = false
+                        }
+                    })
+                binding.optionTwoContainer.animate()
+                    .alpha(0f)
+                    .setDuration(duration / 2)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            binding.optionTwoContainer.isClickable = false
+                        }
+                    })
+                binding.transparentBackground.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
 
-            isOpen = !isOpen
-            if(!isOpen){
-                constraintSetStart.applyTo(binding.anim.container)
-            }else{
-                constraintSetEnd.applyTo(binding.anim.container)
             }
-
         }
     }
-
-
 }
