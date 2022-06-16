a0cf20bd054cc34e276c1d0bef3fe0e3f94b62ce
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-16 22:30:59 +0300

    Урок 8 | setCustomAnimations - простейший способ анимировать переключение фрагментов

--- app/src/main/java/com/gb/m_1919_1872_1/view/ux/UXActivity.kt
+++ app/src/main/java/com/gb/m_1919_1872_1/view/ux/UXActivity.kt
@@ -1,6 +1,7 @@
 package com.gb.m_1919_1872_1.view.ux
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.gb.m_1919_1872_1.R
@@ -33,8 +34,13 @@ class UXActivity : AppCompatActivity() {
         binding.bottomNavigationViewUX.selectedItemId = R.id.fragment_ux_text
     }
 
-    fun navigateTo(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.container,fragment).commit()
+    private fun navigateTo(fragment: Fragment){
+        supportFragmentManager.beginTransaction().setCustomAnimations(
+            R.anim.slide_in,
+            R.anim.fade_out,
+            R.anim.fade_in,
+            R.anim.slide_out
+        ).replace(R.id.container,fragment).commit()
     }
 
 }
