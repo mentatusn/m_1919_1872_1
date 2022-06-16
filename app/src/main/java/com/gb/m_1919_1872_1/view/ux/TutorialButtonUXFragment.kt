6ce271b78f92704db37ca02912d373bfa4b8109b
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-16 22:25:39 +0300

    Урок 8 | туториал

---
+++ app/src/main/java/com/gb/m_1919_1872_1/view/ux/TutorialButtonUXFragment.kt
@@ -0,0 +1,50 @@
+package com.gb.m_1919_1872_1.view.ux
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gb.m_1919_1872_1.databinding.FragmentUxButtonBinding
+import smartdevelop.ir.eram.showcaseviewlib.GuideView
+import smartdevelop.ir.eram.showcaseviewlib.config.DismissType
+
+
+class TutorialButtonUXFragment : Fragment() {
+    private var _binding: FragmentUxButtonBinding? = null
+    private val binding: FragmentUxButtonBinding
+        get() = _binding!!
+
+    companion object{
+        fun newInstance() = TutorialButtonUXFragment()
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        GuideView.Builder(requireContext())
+            .setTitle("Guide Title Text")
+            .setContentText("Guide Description Text\n .....Guide Description Text\n .....Guide Description Text .....")
+            .setTargetView(binding.btnBad)
+            .setDismissType(DismissType.outside) //optional - default dismissible by TargetView
+            .build()
+            .show()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUxButtonBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
